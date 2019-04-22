9337a1fd255b8f1662f86b9f385405bd9112fa51
João Paulo <jpo.oliveiras@gmail.com>
2019-04-22 15:49:00 -0300

    Nova Regra

--- regra.js
+++ regra.js
@@ -20,23 +20,30 @@ while (taxas[i]) {
 document.getElementById("demo").innerHTML = valor_final
 */
 
+
+var tabela = "<td></td>";
+
 function calculo(val1, val2) {
   valor_compra = val1;
   valor_entrada = val2;
 
-  var taxas = [4.6, 6.1, 7.6, 9.1, 10.6, 12.1, 13.6, 15.1, 16.6, 18.1, 19.6, 21.1];
+  var taxas = [4.8, 6.3, 7.8, 9.3, 10.8, 12.3, 13.8, 15.3, 16.8, 18.3, 19.8, 21.3];
   var valor_final = [];
 
-  let valor = valor_compra - valor_entrada;
+  var valor = valor_compra - valor_entrada;
 
   var i = 0;
   while (taxas[i]) {
     
-    let = percentual = (taxas[i] * valor) / 100
+    let percentual = (taxas[i] * valor) / 100
     valor_final.push(valor + percentual);
-    document.getElementById("demo").innerHTML = valor_final
+    document.getElementById("demo").innerHTML = valor_final.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    //return valor_final.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
     i++;
   }
+  console.log(valor_final);
+  return valor_final;
+
 }
 
 var text = document.getElementById("val_compra");
@@ -46,7 +53,7 @@ var demo = document.getElementById("demo");
 text.addEventListener("keyup", function () {
   var val_compra = this.value;
   var val_entrada = document.getElementById("val_entrada").innerHTML
-  calculo(val_compra, val_entrada);
+  var val = calculo(val_compra, val_entrada);
 })
 
 entrada.addEventListener("keyup", function () {
